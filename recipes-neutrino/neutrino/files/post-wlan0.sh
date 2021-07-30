348167dd5830c379707558e146d069924a6f23d9
Markus Volk <f_l_k@t-online.de>
2021-07-30 04:28:26 +0200

    cleanup

--- recipes-neutrino/neutrino/files/post-wlan0.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo Stopping wlan0
- 
-/usr/sbin/wpa_cli terminate
-sleep 2
--- recipes-neutrino/neutrino/files/pre-wlan0.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo Starting wlan0
-
-/usr/sbin/wpa_cli terminate
-sleep 1
-
-wpa_supplicant -B -D nl80211 -i wlan0 -c/etc/wpa_supplicant.conf
