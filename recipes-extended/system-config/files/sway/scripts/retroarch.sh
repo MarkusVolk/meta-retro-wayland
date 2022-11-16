2c4d9a35bd996e81e09ca9364befa5ce24e2b50d
Markus Volk <f_l_k@t-online.de>
2022-11-16 19:46:13 +0100

    retroarch.sh: adjust app_id

--- recipes-extended/system-config/files/sway/scripts/retroarch.sh
+++ recipes-extended/system-config/files/sway/scripts/retroarch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-(ps -e | grep retroarch && swaymsg "[app_id=retroarch] focus";) || \
+(ps -e | grep retroarch && swaymsg "[app_id=org.libretro.RetroArch] focus";) || \
 (swaymsg "workspace $ws2"; exec retroarch;)
